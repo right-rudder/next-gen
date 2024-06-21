0363ea3d608372efd57adb3ecf485f987fe91b8f
Raul Ospina <raminka13@hotmail.com>
2024-06-21 14:41:33 -0500

    Update: add Programs Swiper

--- src/data/training-programs.js
+++ src/data/training-programs.js
@@ -13,45 +13,76 @@ const trainingPrograms = {
       headerH1: "Training Programs",
       headerH2: "Riverside's best flight school",
     },
-
-    //contentOne
-    contentOneImage: "",
-    contentOneImageAlt: "",
-    contentOneImageCaption: "",
-    contentOneH3: "",
-    contentOneParagraphs: ["", ""],
-    contentOneButtonText: "",
-    contentOneButtonLink: "",
-
-    //contentTwo
-    contentTwoImage: "",
-    contentTwoImageAlt: "",
-    contentTwoImageCaption: "",
-    contentTwoH3: "",
-    contentTwoParagraphs: ["", ""],
-    contentTwoButtonText: "",
-    contentTwoButtonLink: "",
-
-    //contentThree
-    contentThreeImage: "",
-    contentThreeImageAlt: "",
-    contentThreeImageCaption: "",
-    contentThreeH3: "",
-    contentThreeParagraphs: ["", ""],
-    contentThreeButtonText: "",
-    contentThreeButtonLink: "",
-
-    //Bottom CTAOne
-    CTAOneH2: "",
-    CTAOneH3: "",
-    CTAOneButtonText: "",
-    CTAOneButtonLink: "",
-
-    //Bottom CTATwo
-    CTATwoH2: "",
-    CTATwoH3: "",
-    CTATwoButtonText: "",
-    CTATwoButtonLink: "",
+    programs: {
+      upperHeading: "Explore",
+      heading: "Our Programs",
+      cards: [
+        {
+          title: "Private Pilot",
+          imagePath: "/src/assets/billy-huynh-v9bnfMCyKbg-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "Instrument Rating",
+          imagePath: "/src/assets/dominik-dancs-3Eqc3Ph4oRg-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "Commercial Pilot",
+          imagePath: "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "CFI",
+          imagePath: "/src/assets/tom-barrett-hgGplX3PFBg-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "Lorem 1",
+          imagePath: "/src/assets/billy-huynh-v9bnfMCyKbg-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "Lorem 2",
+          imagePath: "/src/assets/dominik-dancs-3Eqc3Ph4oRg-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "Lorem 3",
+          imagePath: "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+        {
+          title: "Lorem 4",
+          imagePath: "/src/assets/tom-barrett-hgGplX3PFBg-unsplash.jpg",
+          imageAlt: "Lorem Ipsum Dolor Sit Amet",
+          description:
+            "Lorem Ipsum Dolor Sit Amet, consectetur adipiscing elit.",
+          link: "/training-programs",
+        },
+      ],
+    },
   },
 };
 
