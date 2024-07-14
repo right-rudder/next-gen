850043363177fb8bbb3d4abf64250f2794884be8
Raul Ospina <raminka13@hotmail.com>
2024-07-14 13:20:31 -0500

    New: Aircraft detail page

---
+++ src/data/cessna-172.js
@@ -0,0 +1,59 @@
+const cessna172 = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Cessna 172 - Our Fleet | NextGen Flight Academy",
+    pageDescription:
+      "NextGen Flight Academy offers career-track, zero-to-hero pilot training programs at Riverside Municipal Airport (RAL) and Redlands Municipal Airport (REI). With 320 flying days a year and an FAA-approved Gleim syllabus, our academy provides top-tier education and flexible scheduling options to meet the needs of aspiring pilots. Our programs are designed to ensure high-quality instruction and extensive flying lessons, helping students achieve their goals in flight training efficiently and effectively.",
+    pageKeywords:
+      "NextGen Flight Academy, Flight school Riverside, California; Flight school Redlands, California; Learn to fly Riverside, California; Learn to fly Redlands, California; Pilot training Riverside, California; Pilot training Redlands, California; Flight lessons Riverside, California; Flight lessons Redlands, California; Riverside Redlands flight school; Inland Empire flight school; Flight training; Pilot training; Flight instruction; Flight lessons; Airplane lessons; Private pilot license (PPL) training; Instrument rating training; Commercial pilot license (CPL) training; Certified Flight Instructor (CFI) training",
+
+    //top header
+    header: {
+      imagePath:
+        "/src/assets/in-flight-cfi-and-student-fly-over-los-angeles-ca.jpg",
+      imageAlt: "in-flight-cfi-and-student-fly-over-los-angeles-ca",
+      headerH1: "Cessna 172",
+      headerH2:
+        "Explore our Cessna 172 at NextGen Flight Academy and learn to fly with the best",
+    },
+
+    aircraft: {
+      title: "Cessna 172",
+      normalRate: 154.99,
+      blockRate: 149.99,
+      rentUrl:
+        "https://app.process.st/templates/NextGen-Aircraft-Checkout-qNoL_5yUWfBKy5Jd9PBNaw/checklists/run",
+      documentation: [
+        {
+          docTitle: "Pilot Operating Handbook",
+          docUrl: "/src/assets/cessna-172-poh.pdf",
+        },
+        {
+          docTitle: "Weight & Balance",
+          docUrl: "/src/assets/cessna-172-poh.pdf",
+        },
+        {
+          docTitle: "Renter’s Agreement",
+          docUrl: "/src/assets/cessna-172-poh.pdf",
+        },
+      ],
+      description:
+        "The Cessna 172 Skyhawk is a four-seat, single-engine, high-wing aircraft. Known for its reliability and ease of handling, it is the most popular aircraft for flight training. Its stable flight characteristics, simple control systems, and forgiving landing gear make it ideal for student pilots. Features include a high-wing design for excellent visibility, sturdy tricycle landing gear, and a robust airframe that can withstand the rigors of frequent training flights.",
+      link: "/our-fleet/cessna-172",
+      linkText: "Details",
+    },
+
+    enrollCTA: {
+      heading: "Want a Test Ride?",
+      subheading:
+        "Book an intro flight to see if flight school is right for you",
+      linkText: "Book an Intro Flight",
+      linkUrl: "/intro-flight",
+      imagePath:
+        "/src/assets/nextgen-flight-academy-staff-flying-over-riverside-ca.jpg",
+      imageAlt: "nextgen-flight-academy-staff-flying-over-riverside-ca",
+    },
+  },
+};
+
+export default cessna172;
