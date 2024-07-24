e1a67feb8073d39684d95c98d701fee0f5743d2e
Raul Ospina <raminka13@hotmail.com>
2024-07-24 16:42:38 -0500

    New: pilot resources page

---
+++ src/data/pilot-resource-center.js
@@ -0,0 +1,246 @@
+const pilotResources = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Pilot Resource Center | NextGen Flight Academy",
+    pageDescription:
+      "NextGen Flight Academy offers career-track, zero-to-hero pilot training programs at Riverside Municipal Airport (RAL) and Redlands Municipal Airport (REI). With 320 flying days a year and an FAA-approved Gleim syllabus, our academy provides top-tier education and flexible scheduling options to meet the needs of aspiring pilots. Our programs are designed to ensure high-quality instruction and extensive flying lessons, helping students achieve their goals in flight training efficiently and effectively.",
+    pageKeywords:
+      "NextGen Flight Academy, Flight school Riverside, California; Flight school Redlands, California; Learn to fly Riverside, California; Learn to fly Redlands, California; Pilot training Riverside, California; Pilot training Redlands, California; Flight lessons Riverside, California; Flight lessons Redlands, California; Riverside Redlands flight school; Inland Empire flight school; Flight training; Pilot training; Flight instruction; Flight lessons; Airplane lessons; Private pilot license (PPL) training; Instrument rating training; Commercial pilot license (CPL) training; Certified Flight Instructor (CFI) training",
+
+    //top header
+    header: {
+      imagePath:
+        "/src/assets/in-flight-cfi-and-student-fly-over-los-angeles-ca.jpg",
+      imageAlt: "in-flight-cfi-and-student-fly-over-los-angeles-ca",
+      headerH1: "Pilot Resource Center",
+      headerH2:
+        "Start flying, complete your ratings, and earn your endorsements",
+    },
+    underHeader: {
+      upperHeading: "Featured Program",
+      heading: "Career Track Pilot Training",
+      description: [
+        "Our Career Track program (also known as a “Zero to Hero” program) takes you from no flying experience to becoming a professional pilot. This start-to-finish course prepares you for a career in aviation, offering opportunities in commercial aviation, airlines, and more.",
+        "Submit your enrollment interest today to learn more about our timelines and training structure.",
+      ],
+      imagePath: "/src/assets/redlands-plane-on-dry-lake.jpg",
+      imageAlt: "redlands-plane-on-dry-lake",
+      linkText: "Enroll with NextGen",
+      linkUrl: "/contact",
+    },
+    resources: {
+      upperHeading: "Explore",
+      heading: "FAA Publications",
+      publications: [
+        {
+          name: "Airman Certification Standards (ACS)",
+          cards: [
+            {
+              title: "ACS Private Pilot Airplane",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "ACS Instrument Pilot Airplane",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "https://drive.google.com/file/d/1_6aYUa7tQ7jSbIi8LX91985HjEu14aaS/view?usp=drive_link",
+              linkText: "Download",
+            },
+            {
+              title: "ACS Commercial Pilot Airplane",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "https://drive.google.com/file/d/1ob5c0c1U6PnDm4HS3cDl5owVjfF8wtt_/view?usp=drive_link",
+              linkText: "Download",
+            },
+            {
+              title: "ACS ATP & Type Ratings Certificate",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "https://drive.google.com/file/d/1w_n8OOhf-xei68vqYziRrq53a8kJUqMJ/view?usp=drive_link",
+              linkText: "Download",
+            },
+            {
+              title: "ACS Remote Pilot Small Unmanned",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "https://drive.google.com/file/d/1h104vlc__OmGIT8ToY5qOcVwfrwcMhS0/view?usp=drive_link",
+              linkText: "Download",
+            },
+          ],
+        },
+        {
+          name: "Practical Test Standards (PTS)",
+          cards: [
+            {
+              title: "PTS Flight Instructor Airplane Land and Sea",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "PTS Flight Instructor Instrument Airplane & Helicopter",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "PTS Flight Instructor Airplane Multi-Engine Land and Sea",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+          ],
+        },
+        {
+          name: "Pilot Handbooks",
+          cards: [
+            {
+              title: "Airplane Flying Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Pilot’s Handbook of Aeronautical Knowledge",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Aviation Weather",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Aviation Weather Services",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Weight and Balance Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Advanced Avionics Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Instrument Flying Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Instrument Procedures Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Risk Management Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Aviation Instructor’s Handbook",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+          ],
+        },
+        {
+          name: "GLEIM Syllabus / Miscellaneous",
+          cards: [
+            {
+              title: "FAR/AIM 2021",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Aeronautical Chart User Guide",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Gleim Private Pilot Syllabus",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+            {
+              title: "Gleim Instrument Pilot Syllabus",
+              imagePath:
+                "/src/assets/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+              imageAlt: "passing-a-checkride-at-nextgen-flight-learn-to-fly",
+              link: "/training-programs/private-pilot",
+              linkText: "Download",
+            },
+          ],
+        },
+      ],
+    },
+    enrollCTA: {
+      heading: "Want a Test Ride?",
+      subheading:
+        "Book an intro flight to see if flight school is right for you",
+      linkText: "Book an Intro Flight",
+      linkUrl: "/intro-flight",
+      imagePath:
+        "/src/assets/nextgen-flight-academy-staff-flying-over-riverside-ca.jpg",
+      imageAlt: "nextgen-flight-academy-staff-flying-over-riverside-ca",
+    },
+  },
+};
+
+export default pilotResources;
