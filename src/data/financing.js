9253ea0da66a601871f352bd746138e0e254c6be
Raul Ospina <raminka13@hotmail.com>
2024-07-25 17:23:14 -0500

    New: finance page

---
+++ src/data/financing.js
@@ -0,0 +1,63 @@
+const financing = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Financing | NextGen Flight Academy",
+    pageDescription:
+      "NextGen Flight Academy offers career-track, zero-to-hero pilot training programs at Riverside Municipal Airport (RAL) and Redlands Municipal Airport (REI). With 320 flying days a year and an FAA-approved Gleim syllabus, our academy provides top-tier education and flexible scheduling options to meet the needs of aspiring pilots. Our programs are designed to ensure high-quality instruction and extensive flying lessons, helping students achieve their goals in flight training efficiently and effectively.",
+    pageKeywords:
+      "NextGen Flight Academy, Flight school Riverside, California; Flight school Redlands, California; Learn to fly Riverside, California; Learn to fly Redlands, California; Pilot training Riverside, California; Pilot training Redlands, California; Flight lessons Riverside, California; Flight lessons Redlands, California; Riverside Redlands flight school; Inland Empire flight school; Flight training; Pilot training; Flight instruction; Flight lessons; Airplane lessons; Private pilot license (PPL) training; Instrument rating training; Commercial pilot license (CPL) training; Certified Flight Instructor (CFI) training",
+
+    //top header
+    header: {
+      imagePath:
+        "/src/assets/in-flight-cfi-and-student-fly-over-los-angeles-ca.jpg",
+      imageAlt: "in-flight-cfi-and-student-fly-over-los-angeles-ca",
+      headerH1: "Financing",
+      headerH2:
+        "Learn about financing options and scholarships available to aspiring pilots",
+    },
+
+    finance: [
+      {
+        provider: "Stratus Financial",
+        providerDescription:
+          "NextGen Flight Academy has partnered with Stratus Financial to offer financing packages to take you from your first flight to the airlines. This is our recommended financing option, especially if you’re looking for a complete “zero to hero” package.",
+        imagePath: "/financing/Stratus_financial_logo.webp",
+        imageAlt: "Straus Financial Logo",
+        linkText: "Learn More",
+        linkUrl: "https://stratus.finance/apply/nextgenflightacademy/",
+      },
+      {
+        provider: "Flight Training Finance",
+        providerDescription:
+          "Our easy, affordable programs are for part-time training. They are available at most flight schools nationwide. Chances are, you will find us right at your nearby airport and can begin flying right away.",
+        imagePath: "/financing/flight-training-finance-1.webp",
+        imageAlt: "Flight Training Finance Logo",
+        linkText: "Learn More",
+        linkUrl: "https://www.flighttrainingfinancellc.com/",
+      },
+      {
+        provider: "Surv Credit",
+        providerDescription:
+          "We work with the most advanced lending solutions to provide you the financing you need.",
+        imagePath: "/financing/surv-credit-logo.webp",
+        imageAlt: "Surv Credit Logo",
+        linkText: "Learn More",
+        linkUrl: "https://www.yourfinancingnow.com/ngfa/",
+      },
+    ],
+
+    enrollCTA: {
+      heading: "Want a Test Ride?",
+      subheading:
+        "Book an intro flight to see if flight school is right for you",
+      linkText: "Book an Intro Flight",
+      linkUrl: "/intro-flight",
+      imagePath:
+        "/src/assets/nextgen-flight-academy-staff-flying-over-riverside-ca.jpg",
+      imageAlt: "nextgen-flight-academy-staff-flying-over-riverside-ca",
+    },
+  },
+};
+
+export default financing;
