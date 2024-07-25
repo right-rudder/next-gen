c7394f0114fca39a836821913d5b0bea74909215
Raul Ospina <raminka13@hotmail.com>
2024-07-25 17:23:37 -0500

    New: partners page

---
+++ src/data/partnerships.js
@@ -0,0 +1,63 @@
+const partnerships = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Partnerships | NextGen Flight Academy",
+    pageDescription:
+      "NextGen Flight Academy offers career-track, zero-to-hero pilot training programs at Riverside Municipal Airport (RAL) and Redlands Municipal Airport (REI). With 320 flying days a year and an FAA-approved Gleim syllabus, our academy provides top-tier education and flexible scheduling options to meet the needs of aspiring pilots. Our programs are designed to ensure high-quality instruction and extensive flying lessons, helping students achieve their goals in flight training efficiently and effectively.",
+    pageKeywords:
+      "NextGen Flight Academy, Flight school Riverside, California; Flight school Redlands, California; Learn to fly Riverside, California; Learn to fly Redlands, California; Pilot training Riverside, California; Pilot training Redlands, California; Flight lessons Riverside, California; Flight lessons Redlands, California; Riverside Redlands flight school; Inland Empire flight school; Flight training; Pilot training; Flight instruction; Flight lessons; Airplane lessons; Private pilot license (PPL) training; Instrument rating training; Commercial pilot license (CPL) training; Certified Flight Instructor (CFI) training",
+
+    //top header
+    header: {
+      imagePath:
+        "/src/assets/in-flight-cfi-and-student-fly-over-los-angeles-ca.jpg",
+      imageAlt: "in-flight-cfi-and-student-fly-over-los-angeles-ca",
+      headerH1: "Partnerships",
+      headerH2:
+        "NextGen Flight Academy has partnered with the best in the industry to provide you with the best flight training experience.",
+    },
+
+    partners: [
+      {
+        provider: "Spartan's 15-15 Program",
+        providerDescription:
+          "Looking to earn your bachelor’s degree while continuing to fly at your local school? Spartan College combines technical training with education in communication, quantitative reasoning, critical thinking, and business management to prepare you for challenges in the field. The training, experience, and education you receive with our Bachelor of Science in Technology Management degree increase your knowledge and management skills. Graduates of this program are well-rounded and prepared for future opportunities, including entry-level management.",
+        imagePath: "/financing/Stratus_financial_logo.webp",
+        imageAlt: "Straus Financial Logo",
+        linkText: "Learn More",
+        linkUrl: "https://stratus.finance/apply/nextgenflightacademy/",
+      },
+      {
+        provider: "American English Institute",
+        providerDescription:
+          "Students enroll in AEI’s Intensive English Program to improve their English for flight training while studying aviation English as an elective to learn aviation vocabulary and flight training concepts. Students are tested at the start of the program to determine their English proficiency level. The amount of English language instruction required during the program depends on a student’s incoming level. Each level requires 8 weeks to complete.",
+        imagePath: "/financing/Stratus_financial_logo.webp",
+        imageAlt: "Straus Financial Logo",
+        linkText: "Learn More",
+        linkUrl: "https://stratus.finance/apply/nextgenflightacademy/",
+      },
+      {
+        provider: "Excel Charter Academy School",
+        providerDescription:
+          "Excel Charter Academy School is an independent study program for students. We have partnered up with them to give your child flight lessons. This program allows students to get flight lessons in our simulator with a certified instructor. The lessons are paid for through the school and labeled as an extracurricular activity. It also counts towards the student's school credit.",
+        imagePath: "/financing/Stratus_financial_logo.webp",
+        imageAlt: "Straus Financial Logo",
+        linkText: "Learn More",
+        linkUrl: "https://stratus.finance/apply/nextgenflightacademy/",
+      },
+    ],
+
+    enrollCTA: {
+      heading: "Want a Test Ride?",
+      subheading:
+        "Book an intro flight to see if flight school is right for you",
+      linkText: "Book an Intro Flight",
+      linkUrl: "/intro-flight",
+      imagePath:
+        "/src/assets/nextgen-flight-academy-staff-flying-over-riverside-ca.jpg",
+      imageAlt: "nextgen-flight-academy-staff-flying-over-riverside-ca",
+    },
+  },
+};
+
+export default partnerships;
