9bd5a6775f5b908e8ee50c088e8a1fddab16bf7e
Charles Ferguson <chazferg11@gmail.com>
2024-07-26 12:29:28 -0300

    image and about page

--- src/data/about.js
+++ src/data/about.js
@@ -1,30 +1,31 @@
 const about = {
   data: {
     //SEO Header Stuff
-    pageTitle: "Our Story | NextGen Flight Academy",
+    pageTitle: "About | NextGen Flight Academy",
     pageDescription:
-      "NextGen Flight Academy offers career-track, zero-to-hero pilot training programs at Riverside Municipal Airport (RAL) and Redlands Municipal Airport (REI). With 320 flying days a year and an FAA-approved Gleim syllabus, our academy provides top-tier education and flexible scheduling options to meet the needs of aspiring pilots. Our programs are designed to ensure high-quality instruction and extensive flying lessons, helping students achieve their goals in flight training efficiently and effectively.",
+      "Discover NextGen Flight Academy at Riverside Municipal Airport and Learn to Fly at Redlands Municipal Airport, where aspiring pilots receive top-quality flight training. We offer flexible and career track training programs from Private Pilot to ATP with a 95% checkride pass rate. Join us to achieve your aviation goals with our experienced instructors and FAA-approved Gleim syllabus.",
     pageKeywords:
-      "NextGen Flight Academy, Flight school Riverside, California; Flight school Redlands, California; Learn to fly Riverside, California; Learn to fly Redlands, California; Pilot training Riverside, California; Pilot training Redlands, California; Flight lessons Riverside, California; Flight lessons Redlands, California; Riverside Redlands flight school; Inland Empire flight school; Flight training; Pilot training; Flight instruction; Flight lessons; Airplane lessons; Private pilot license (PPL) training; Instrument rating training; Commercial pilot license (CPL) training; Certified Flight Instructor (CFI) training",
+      "NextGen Flight Academy, Learn to Fly, flight training, Riverside Municipal Airport, Redlands Municipal Airport, Private Pilot, ATP, CFI, CFII, MEI, FAA-approved Gleim syllabus, 95% checkride pass rate, aviation training, pilot programs, Southern California flight school, flexible flight training, plane rentals, demo flights, aviation education, career track pilot training",
 
     //top header
     header: {
       imagePath:
-        "/src/assets/in-flight-cfi-and-student-fly-over-los-angeles-ca.jpg",
+        "/src/assets/nextgen-flight-academy-cfi-team-riverside-learn-to-fly-redlands.jpg",
       imageAlt: "in-flight-cfi-and-student-fly-over-los-angeles-ca",
-      headerH1: "Our Story",
+      headerH1: "About NextGen",
       headerH2:
-        "We are passionate about aviation and dedicated to helping you achieve your goals",
+        "Learn to Fly in Redlands and Riverside, CA",
     },
 
     ourStory: {
       imagePath:
-        "/src/assets/nextgen-flight-academy-staff-flying-over-riverside-ca.jpg",
+        "/src/assets/nextgen-flight-academy-cfi-team-riverside-learn-to-fly-redlands.jpg",
       imageAlt: "nextgen-flight-academy-staff-flying-over-riverside-ca",
       about: [
-        "NextGen Flight Academy is a flight school located at Riverside Municipal Airport (RAL) and Redlands Municipal Airport (REI) in Southern California. Our mission is to provide aspiring pilots with the highest quality flight training and education, helping them achieve their goals efficiently and effectively.",
-        "Our academy offers career-track, zero-to-hero pilot training programs that are designed to ensure high-quality instruction and extensive flying lessons. We provide top-tier education and flexible scheduling options to meet the needs of our students, with 320 flying days a year and an FAA-approved Gleim syllabus.",
-        "At NextGen Flight Academy, we are passionate about aviation and dedicated to helping you achieve your goals. Whether you are pursuing a private pilot license (PPL), instrument rating, commercial pilot license (CPL), certified flight instructor (CFI) rating, or airline transport pilot (ATP) certificate, we are here to support you every step of the way.",
+        "Welcome to NextGen Flight Academy, where we are dedicated to providing aspiring pilots with top-quality flight training. Located at Riverside Municipal Airport (RAL), we also operate our sister school, Learn to Fly, at Redlands Municipal Airport (REI). Our mission is to help you achieve your aviation goals efficiently and effectively through top-quality education and extensive flying lessons.",
+        "At our Riverside location, we offer a wide range of programs from Private Pilot through ATP, including CFI, CFII, and MEI. Over at Learn to Fly in Redlands, we provide training from Private through CFII.",
+        "With 320 flying days a year and flexible instructor scheduling, we accommodate the diverse needs of our students. Our FAA-approved Gleim syllabus ensures high-quality instruction, and our family-oriented approach makes us easy to work with. Our instructors are available before and after office hours, providing a wide array of availability for all students.",
+        "Join us at NextGen Flight Academy and Learn to Fly. Embark on your aviation journey with a team dedicated to your success every step of the way.",
       ],
       quotes: [
         "Our mission is to provide aspiring pilots with the highest quality flight training and education, helping them achieve their goals efficiently and effectively.",
@@ -39,7 +40,7 @@ const about = {
       linkText: "Book an Intro Flight",
       linkUrl: "/intro-flight",
       imagePath:
-        "/src/assets/nextgen-flight-academy-staff-flying-over-riverside-ca.jpg",
+        "/src/assets/student-pilot-takes-piper-cherokee-out-from-nextgen-flight-academy-redlands-riverside-ca-pilot-training-school.jpg",
       imageAlt: "nextgen-flight-academy-staff-flying-over-riverside-ca",
     },
   },
