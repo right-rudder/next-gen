a8c3514b34889af4d617c5ed2cb551611c090857
lucascr <lcrodrigues1995@gmail.com>
2025-04-28 12:09:03 -0300

    Update CounterCard.jsx

--- src/components/CounterCard.jsx
+++ src/components/CounterCard.jsx
@@ -2,25 +2,20 @@ import CountUp from "react-countup";
 
 const Counter = ({ value, title }) => {
   return (
-    <nav>
-      <div className="bg-dark-blue flex flex-col py-10 group">
-        <div className="border-r border-white/20 px-10 flex flex-col items-center gap-1 group-last:border-0">
-          <span className="text-5xl md:text-7xl font-medium">
-            <span className="sr-only">{value}</span>
-            <CountUp
-              end={value}
-              enableScrollSpy
-              scrollSpyDelay={100}
-              scrollSpyOnce
-            />
-            <span className="text-main-blue">{ value <= 100 ? "%" : "+" }</span>
-          </span>
-          <h2 className="text-lg text-center whitespace-nowrap">
-            {title}
-          </h2>
-        </div>
+    <div className="flex flex-col bg-slate-950 items-center text-center flex-1 min-w-[150px]">
+      <div className="border-r border-white/20 last:border-0 px-6 sm:px-8 py-6">
+        <span className="text-4xl sm:text-5xl md:text-6xl font-bold text-white">
+          <CountUp
+            end={value}
+            enableScrollSpy
+            scrollSpyDelay={100}
+            scrollSpyOnce
+          />
+          <span className="text-main-blue">{value <= 100 ? "%" : "+"}</span>
+        </span>
+        <h2 className="mt-3 text-base sm:text-lg text-white">{title}</h2>
       </div>
-    </nav>
+    </div>
   );
 };
 
